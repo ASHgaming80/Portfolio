8ed558b9961ca9ea2d0e10a771576a8cbc714d4b
ASH <84627566+ASHgaming80@users.noreply.github.com>
2022-12-17 12:39:33 +0530

    Create contact.php

---
+++ contact.php
@@ -0,0 +1,18 @@
+<?php
+//variable name
+$name = $_REQUEST['name'];
+$email = $_REQUEST['email'];
+$message = $_REQUEST['message'];
+
+//check input fields
+if (empty($name)) || (empty($email)) || (empty($message))
+{
+    echo "Please fill all the fields";
+}
+else
+{
+    mail("ashgaming1980@gmail.com", "Contact Form Message", $message, "From: $name <$email>");
+    echo "<script type= 'text/javascript'>alert('Your Message is sent successfully')</script>";
+    window.history.log(-1);
+}
+?>
